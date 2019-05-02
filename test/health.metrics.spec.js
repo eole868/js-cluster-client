0ddc9ab3d86ed06ca3161d54804efe17dd13e9e2
vasa <vasa@towardsblockchain.com>
2019-05-03 02:43:54 +0530

    added new test cases

---
+++ test/health.metrics.spec.js
@@ -0,0 +1,11 @@
+const cluster = require('./helpers')
+const assert = require('chai').assert
+
+/* describe('health.metrics', () => {
+    it('lists latest metrics logged by this peer', (done) => {
+        cluster.health.metrics('name', {}, (err, health) => {
+            assert.notExists(err, 'throws error while listing latest metrics logged by this peer')
+            done()
+        })
+    })
+}) */
