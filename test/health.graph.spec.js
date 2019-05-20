e3ab662e680692a1232c6818b8006235403c3bc9
vasa <vasa@towardsblockchain.com>
2019-05-21 00:56:16 +0530

    test: update tests
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@towardsblockchain.com>

--- test/health.graph.spec.js
+++ test/health.graph.spec.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert
 
 describe('health.graph', () => {
     it('creates a graph displaying connectivity of cluster peers (without options)', (done) => {
-        cluster.health.graph({}, (err, health) => {
+        cluster.health.graph((err, health) => {
             assert.notExists(err, 'throws error while creating a graph displaying connectivity of cluster peers (without options)')
             done()
         })
